620916d1ffe0226261211b65a6b79b97c06c9e9c
dino9611 <aldinorahman36@gmail.com>
2021-07-28 12:00:44 +0700

    day 3 sesi 1

--- exercise.js
+++ exercise.js
@@ -1,3 +1,18 @@
-
+var berat = 67 // dalam kg
+var tinggi = 1.78 // dalam meter
+var imt = berat / (tinggi ** 2)
+var status
+if (imt < 18.5) {
+    status = 'berat badan kurang'
+} else if (imt >= 18.5 && imt < 25) {
+    status = 'berat badan ideal'
+} else if (imt >= 25 && imt < 30) {
+    status = 'bb berlebih'
+} else if (imt >= 30 && imt < 40) {
+    status = 'bb terlalu berlebih'
+} else {
+    status = 'obesitas'
+}
+console.log(`imt = ${imt}, ${status}`)
 
 
